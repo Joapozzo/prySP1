633adfb49e330d02a2132ab846b448e41ba87374
root <mgiraudo@ies21.edu.ar>
2023-03-30 13:54:34 -0300

    Agregar archivos de proyecto.

---
+++ ClsRepuesto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pryClassVeterinaria
+{
+    class ClsRepuesto
+    {
+        public int Codigo {get; set;}
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public decimal Precio { get; set; }
+        public bool Origen { get; set; }
+
+        public void Consultar()
+        {
+
+        }
+   
+    }
+}
