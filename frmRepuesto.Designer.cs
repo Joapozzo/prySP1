90e87b0110a093a3e0444671dc6acb221bf558a6
Joapozzo <pozzojoa@gmail.com>
2023-04-24 18:46:41 -0300

    24/04

--- frmRepuesto.Designer.cs
+++ frmRepuesto.Designer.cs
@@ -62,9 +62,11 @@ namespace pryClassVeterinaria
             this.mrcRepuesto.Controls.Add(this.lblMarca);
             this.mrcRepuesto.Controls.Add(this.lblNombre);
             this.mrcRepuesto.Controls.Add(this.lblCodigo);
-            this.mrcRepuesto.Location = new System.Drawing.Point(12, 23);
+            this.mrcRepuesto.Location = new System.Drawing.Point(16, 28);
+            this.mrcRepuesto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mrcRepuesto.Name = "mrcRepuesto";
-            this.mrcRepuesto.Size = new System.Drawing.Size(292, 264);
+            this.mrcRepuesto.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.mrcRepuesto.Size = new System.Drawing.Size(389, 325);
             this.mrcRepuesto.TabIndex = 0;
             this.mrcRepuesto.TabStop = false;
             this.mrcRepuesto.Text = "Repuesto";
@@ -72,9 +74,10 @@ namespace pryClassVeterinaria
             // optImportado
             // 
             this.optImportado.AutoSize = true;
-            this.optImportado.Location = new System.Drawing.Point(170, 210);
+            this.optImportado.Location = new System.Drawing.Point(227, 258);
+            this.optImportado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.optImportado.Name = "optImportado";
-            this.optImportado.Size = new System.Drawing.Size(72, 17);
+            this.optImportado.Size = new System.Drawing.Size(89, 20);
             this.optImportado.TabIndex = 11;
             this.optImportado.Text = "Importado";
             this.optImportado.UseVisualStyleBackColor = true;
@@ -83,9 +86,10 @@ namespace pryClassVeterinaria
             // 
             this.optNacional.AutoSize = true;
             this.optNacional.Checked = true;
-            this.optNacional.Location = new System.Drawing.Point(79, 210);
+            this.optNacional.Location = new System.Drawing.Point(105, 258);
+            this.optNacional.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.optNacional.Name = "optNacional";
-            this.optNacional.Size = new System.Drawing.Size(67, 17);
+            this.optNacional.Size = new System.Drawing.Size(82, 20);
             this.optNacional.TabIndex = 10;
             this.optNacional.TabStop = true;
             this.optNacional.Text = "Nacional";
@@ -94,86 +98,107 @@ namespace pryClassVeterinaria
             // lblOrigen
             // 
             this.lblOrigen.AutoSize = true;
-            this.lblOrigen.Location = new System.Drawing.Point(17, 210);
+            this.lblOrigen.Location = new System.Drawing.Point(23, 258);
+            this.lblOrigen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblOrigen.Name = "lblOrigen";
-            this.lblOrigen.Size = new System.Drawing.Size(38, 13);
+            this.lblOrigen.Size = new System.Drawing.Size(47, 16);
             this.lblOrigen.TabIndex = 9;
             this.lblOrigen.Text = "Origen";
             // 
             // cmbMarca
             // 
+            this.cmbMarca.Enabled = false;
             this.cmbMarca.FormattingEnabled = true;
             this.cmbMarca.Items.AddRange(new object[] {
             "Marca A",
             "Marca B",
             "Marca C"});
-            this.cmbMarca.Location = new System.Drawing.Point(69, 125);
+            this.cmbMarca.Location = new System.Drawing.Point(92, 154);
+            this.cmbMarca.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cmbMarca.Name = "cmbMarca";
-            this.cmbMarca.Size = new System.Drawing.Size(173, 21);
+            this.cmbMarca.Size = new System.Drawing.Size(229, 24);
             this.cmbMarca.TabIndex = 8;
+            this.cmbMarca.SelectedIndexChanged += new System.EventHandler(this.cmbMarca_SelectedIndexChanged);
             // 
             // txtPrecio
             // 
-            this.txtPrecio.Location = new System.Drawing.Point(69, 166);
+            this.txtPrecio.Enabled = false;
+            this.txtPrecio.Location = new System.Drawing.Point(92, 204);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.Size = new System.Drawing.Size(132, 22);
             this.txtPrecio.TabIndex = 7;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.txtPrecio_TextChanged);
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(69, 82);
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(92, 101);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(173, 20);
+            this.txtNombre.Size = new System.Drawing.Size(229, 22);
             this.txtNombre.TabIndex = 5;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            this.txtNombre.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNombre_KeyPress);
             // 
             // txtCodigo
             // 
-            this.txtCodigo.Location = new System.Drawing.Point(69, 40);
+            this.txtCodigo.Location = new System.Drawing.Point(92, 49);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCodigo.Name = "txtCodigo";
-            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.Size = new System.Drawing.Size(132, 22);
             this.txtCodigo.TabIndex = 4;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            this.txtCodigo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCodigo_KeyPress);
             // 
             // lblPrecio
             // 
             this.lblPrecio.AutoSize = true;
-            this.lblPrecio.Location = new System.Drawing.Point(16, 169);
+            this.lblPrecio.Location = new System.Drawing.Point(21, 208);
+            this.lblPrecio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPrecio.Name = "lblPrecio";
-            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.Size = new System.Drawing.Size(46, 16);
             this.lblPrecio.TabIndex = 3;
             this.lblPrecio.Text = "Precio";
             // 
             // lblMarca
             // 
             this.lblMarca.AutoSize = true;
-            this.lblMarca.Location = new System.Drawing.Point(16, 125);
+            this.lblMarca.Location = new System.Drawing.Point(21, 154);
+            this.lblMarca.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMarca.Name = "lblMarca";
-            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.Size = new System.Drawing.Size(45, 16);
             this.lblMarca.TabIndex = 2;
             this.lblMarca.Text = "Marca";
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(16, 82);
+            this.lblNombre.Location = new System.Drawing.Point(21, 101);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.Size = new System.Drawing.Size(56, 16);
             this.lblNombre.TabIndex = 1;
             this.lblNombre.Text = "Nombre";
             // 
             // lblCodigo
             // 
             this.lblCodigo.AutoSize = true;
-            this.lblCodigo.Location = new System.Drawing.Point(16, 43);
+            this.lblCodigo.Location = new System.Drawing.Point(21, 53);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCodigo.Name = "lblCodigo";
-            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.Size = new System.Drawing.Size(51, 16);
             this.lblCodigo.TabIndex = 0;
             this.lblCodigo.Text = "Codigo";
             // 
             // btnRegistrar
             // 
-            this.btnRegistrar.Location = new System.Drawing.Point(332, 40);
+            this.btnRegistrar.Enabled = false;
+            this.btnRegistrar.Location = new System.Drawing.Point(443, 49);
+            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(140, 30);
+            this.btnRegistrar.Size = new System.Drawing.Size(187, 37);
             this.btnRegistrar.TabIndex = 1;
             this.btnRegistrar.Text = "Registrar";
             this.btnRegistrar.UseVisualStyleBackColor = true;
@@ -181,9 +206,11 @@ namespace pryClassVeterinaria
             // 
             // btnCancelar
             // 
-            this.btnCancelar.Location = new System.Drawing.Point(332, 105);
+            this.btnCancelar.Enabled = false;
+            this.btnCancelar.Location = new System.Drawing.Point(443, 129);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(140, 30);
+            this.btnCancelar.Size = new System.Drawing.Size(187, 37);
             this.btnCancelar.TabIndex = 2;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = true;
@@ -191,9 +218,11 @@ namespace pryClassVeterinaria
             // 
             // btnConsultar
             // 
-            this.btnConsultar.Location = new System.Drawing.Point(332, 165);
+            this.btnConsultar.Enabled = false;
+            this.btnConsultar.Location = new System.Drawing.Point(443, 203);
+            this.btnConsultar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnConsultar.Name = "btnConsultar";
-            this.btnConsultar.Size = new System.Drawing.Size(140, 29);
+            this.btnConsultar.Size = new System.Drawing.Size(187, 36);
             this.btnConsultar.TabIndex = 3;
             this.btnConsultar.Text = "Consultar";
             this.btnConsultar.UseVisualStyleBackColor = true;
@@ -201,9 +230,10 @@ namespace pryClassVeterinaria
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(332, 230);
+            this.btnSalir.Location = new System.Drawing.Point(443, 283);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(140, 30);
+            this.btnSalir.Size = new System.Drawing.Size(187, 37);
             this.btnSalir.TabIndex = 4;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
@@ -212,23 +242,26 @@ namespace pryClassVeterinaria
             // lstLista
             // 
             this.lstLista.FormattingEnabled = true;
-            this.lstLista.Location = new System.Drawing.Point(12, 302);
+            this.lstLista.ItemHeight = 16;
+            this.lstLista.Location = new System.Drawing.Point(16, 372);
+            this.lstLista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lstLista.Name = "lstLista";
-            this.lstLista.Size = new System.Drawing.Size(292, 121);
+            this.lstLista.Size = new System.Drawing.Size(388, 148);
             this.lstLista.TabIndex = 5;
             this.lstLista.Tag = "";
             // 
             // frmRepuesto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(498, 466);
+            this.ClientSize = new System.Drawing.Size(664, 574);
             this.Controls.Add(this.lstLista);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnConsultar);
             this.Controls.Add(this.btnCancelar);
             this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.mrcRepuesto);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmRepuesto";
             this.Text = "Repuesto";
             this.Load += new System.EventHandler(this.frmRepuesto_Load);
--- frmRepuesto.cs
+++ frmRepuesto.cs
@@ -52,6 +52,8 @@ namespace pryClassVeterinaria
                 objRepuesto.Origen = "Nacional";
             }
 
+            
+
             MessageBox.Show(objRepuesto.ObtenerDatos());
 
             listaRepuestos.Add(objRepuesto);
@@ -64,7 +66,7 @@ namespace pryClassVeterinaria
             {
                 lstLista.Items.Add("Nombre:  " + repuesto.Nombre + "   Codigo:  " + repuesto.Codigo.ToString());
             }
-            
+
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
@@ -76,5 +78,88 @@ namespace pryClassVeterinaria
         {
             LimpiarDatos();
         }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            if (txtNombre.Text != "")
+            {
+                cmbMarca.Enabled = true;
+            }
+            else
+            {
+                cmbMarca.Enabled = false;
+            }
+        }
+
+        private void cmbMarca_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbMarca.SelectedIndex != -1)
+            {
+                txtPrecio.Enabled = true;
+            }
+            else
+            {
+                txtPrecio.Enabled = false;
+            }
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            if (txtCodigo.Text != "")
+            {
+                txtNombre.Enabled = true;
+            }
+            else
+            {
+                txtNombre.Enabled = false;
+            }
+        }
+
+        private void txtPrecio_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPrecio.Text != "")
+            {
+                btnRegistrar.Enabled = true;
+                btnConsultar.Enabled = true;
+                btnCancelar.Enabled = true;
+            }
+            else
+            {
+                btnRegistrar.Enabled = false;
+                btnConsultar.Enabled = false;
+                btnCancelar.Enabled = false;
+            }
+        }
+
+        private void txtCodigo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("INGRESE UNICAMENTE NUMEROS", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtNombre_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 64) || (e.KeyChar >= 91 && e.KeyChar <= 96) || (e.KeyChar >= 123 && e.KeyChar <= 255)) 
+            {
+                MessageBox.Show("INGRESE UNICAMENTE LETRAS", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
+
+        private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar >= 32 && e.KeyChar <= 47) || (e.KeyChar >= 58 && e.KeyChar <= 255))
+            {
+                MessageBox.Show("INGRESE UNICAMENTE NUMEROS", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                e.Handled = true;
+                return;
+            }
+        }
     }
+
 }
